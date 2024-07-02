f7886f0cc5af844edfccfaa99e42d59d6115be96
murtaza-simform <murtaza.khachrodwala@simformsolutions.com>
2024-07-02 15:56:04 +0530

    Error handling in nextJs

--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,7 +1,16 @@
-import React from "react";
+"use client"
+import React, { useEffect, useState } from "react";
 
 const ProductDetails = ({ params }: { params: { productId: string } }) => {
-  return <div>Product Detail {params?.productId}</div>;
+  const [timer,setTimer] = useState<any>(0);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setTimer(1)
+    }, 5000)
+  }, []);
+
+  return timer>=1 &&  <div>Product Detail {params?.productId}</div>;
 };
 
 export default ProductDetails;
--- src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+
 type Props ={
     params: {
         productId: string;
@@ -6,6 +9,17 @@ type Props ={
 }
 
 export default function ReviewPage({params}: Props) {
+
+    const random= Math.floor(Math.random() *2);
+
+    if(random===1){
+        throw new Error("Some random error occured!")
+    }
+
+    if(+params?.reviewId>5){
+        throw new Error("Review Id cannot be greater that 5")
+    }
+
     return (
         <div><h1>Review {params?.reviewId} for product {params?.productId}</h1></div>
     );
